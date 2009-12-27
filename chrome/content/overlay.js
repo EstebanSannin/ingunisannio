14644675e3e5313d8e61245484341925c1a2451a
Ivan Morgillo <imorgillo [at] sanniolug [dot] org>
2009-12-28 07:05:35 +0800

    Riscrittura parziale dell'overlay

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -18,106 +18,111 @@ Author: Stefano Viola < stefanoviola [at] sanniolug [dot] org >
 Collaborator: Ivan Morgillo < imorgillo [at] sanniolug [dot] org >
 */
 
-//Global variables
-var titles;
-var avvisi;
-var links;
-var initList;
-var arrayDate = new Array();
-var allDescription;
-var descriptionArray = new Array();
-var avvisiarray = new Array();
-const url = "http://www.ing.unisannio.it/avvisi/rss20.xml";
+var ingunisannio = {
 
-//Funzione per la finestra About:
-function aboutEsteban() {
-  window.openDialog("chrome://ingunisannio/content/about.xul");
-}
-function savvisi(){
-  window.openDialog("chrome://ingunisannio/content/showAvvisi.xul");
-}
+    titles: '',
+    avvisi: '',
+    links: '',
+    initList: '',
+    arrayDate: [],
+    allDescription: '',
+    descriptionArray: [],
+    avvisiarray: [],
+    url: "http://www.ing.unisannio.it/avvisi/rss20.xml",
 
-function appendToList(element, index, array) {
-  
-  initList.appendItem(element, element);
-}
-
-function getAvvisi(){
-  var tempAvvisi = "";
-  var description;
-  var tempDate = "";
-  var titles;
-  var tempDesc = "";
-  var date;
-  var req = new XMLHttpRequest();  
-  req.open('GET', url, false);   
-  req.send(null);
-  //alert(req.status); for debug
-  if(req.status == 200){
+    onLoad: function() {
+	// Do some initialization
+    },
     
-    var xmldoc = req.responseXML;
-    titles = xmldoc.getElementsByTagName("title");
-    date = xmldoc.getElementsByTagName("pubDate");
-    description = xmldoc.getElementsByTagName("description");
-   
-  }  
-  else
-    alert("Error loading page Req status:" + req.status + " and url: " + url);  
+    about: function() {
+	window.openDialog("chrome://ingunisannio/content/about.xul");
+    },
+    
+    showAvvisi: function() {
+	window.openDialog("chrome://ingunisannio/content/showAvvisi.xul");
+    },
 
-  for(var i=1; i<20; i++){
-    //tempDesc+= description[i].textContent +"\n"; 
-    //tempAvvisi+= titles[i].textContent +"\n";
-    //tempDate+= date[i].textContent +"\n";
-    arrayDate[i] = date[i-1].textContent;
-    avvisiarray[i] = titles[i].textContent;
-    descriptionArray[i] = description[i].textContent;
-  }
-  // allDescription = tempDesc;
-  avvisi = tempAvvisi;      
-  initList = document.getElementById('multiline');   
-  var countItem = initList.getRowCount();
-  
-  if(countItem == 0){
-    avvisiarray.forEach(appendToList);
- }
- else{
-   for(var j=0; j<countItem; j++){
-     initList.removeItemAt(0);
-   }
-   avvisiarray.forEach(appendToList);
-   }
-  
+    appendToList: function(element, index, array) {
+	initList.appendItem(element, element);
+    },
+
+    getAvvisi: function() {
+	var tempAvvisi = "";
+	var description;
+	var tempDate = "";
+	var titles;
+	var tempDesc = "";
+	var date;
+	var req = new XMLHttpRequest();  
+	req.open('GET', url, false);   
+	req.send(null);
+	//alert(req.status); for debug
+	if(req.status == 200){
+	    
+	    var xmldoc = req.responseXML;
+	    titles = xmldoc.getElementsByTagName("title");
+	    date = xmldoc.getElementsByTagName("pubDate");
+	    description = xmldoc.getElementsByTagName("description");
+	    
+	}  
+	else
+	    alert("Error loading page Req status:" + req.status + " and url: " + url);  
+
+	for(var i=1; i<20; i++){
+	    //tempDesc+= description[i].textContent +"\n"; 
+	    //tempAvvisi+= titles[i].textContent +"\n";
+	    //tempDate+= date[i].textContent +"\n";
+	    arrayDate[i] = date[i-1].textContent;
+	    avvisiarray[i] = titles[i].textContent;
+	    descriptionArray[i] = description[i].textContent;
+	}
+	// allDescription = tempDesc;
+	avvisi = tempAvvisi;      
+	initList = document.getElementById('multiline');   
+	var countItem = initList.getRowCount();
+	
+	if(countItem == 0){
+	    avvisiarray.forEach(appendToList);
+	}
+	else{
+	    for(var j=0; j<countItem; j++){
+		initList.removeItemAt(0);
+	    }
+	    avvisiarray.forEach(appendToList);
+	}
+	
+    },
+
+    setText: function(listID,textID) {
+	var listBox = document.getElementById(listID);
+	var selectedItem = listBox.getSelectedItem(0);
+	var count = listBox.currentIndex;
+	var newText = selectedItem.getAttribute("label");
+	var str = descriptionArray[count+1];
+	var temp = str.replace("<br />", "", "gi");
+	var dataAvvisi = arrayDate[count+1];
+	document.getElementById("description-box").value = temp;
+	document.getElementById("date-box").value = dataAvvisi.replace("+0100", "", "gi");
+    },
+
+    goToStudentportal: function() {
+	gBrowser.addTab("https://servizistudenti.unisannio.it:4446/pls/self/gissweb.auth?p_cod_lingua=004&p_tipoutente=&p_opzione=");
+    },
+
+    goToForum: function() {
+	gBrowser.addTab("http://unibn.altervista.org");
+    }
+
+    // Add tab:  
+    //gBrowser.addTab("http://www.google.com/");
+
+    // Add tab, then make active:
+    //gBrowser.selectedTab = gBrowser.addTab("http://www.google.com/");
 }
 
-function setText(listID,textID)
-{
-  var listBox = document.getElementById(listID);
-  var selectedItem = listBox.getSelectedItem(0);
-  var count = listBox.currentIndex;
-  var newText = selectedItem.getAttribute("label");
-  var str = descriptionArray[count+1];
-  var temp = str.replace("<br />", "", "gi");
-  var dataAvvisi = arrayDate[count+1];
-  document.getElementById("description-box").value = temp;
-  document.getElementById("date-box").value = dataAvvisi.replace("+0100", "", "gi");
-}
-
-function studentPortal(){
-  gBrowser.addTab("https://servizistudenti.unisannio.it:4446/pls/self/gissweb.auth?p_cod_lingua=004&p_tipoutente=&p_opzione=");
-}
-
-function forum(){
-  gBrowser.addTab("http://unibn.altervista.org");
-}
-
-
-// Add tab:  
-//gBrowser.addTab("http://www.google.com/");
-
-// Add tab, then make active:
-//gBrowser.selectedTab = gBrowser.addTab("http://www.google.com/");
-
-
-
-
-
+// ON LOAD
+window.addEventListener("load", function(e) {
+    // Load main object
+    ingunisannio.onLoad(e);
+    
+}, false);
