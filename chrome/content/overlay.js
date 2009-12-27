de3281992b04ab78ee7bdaf86735e023b403b7c1
Ivan Morgillo <imorgillo [at] sanniolug [dot] org>
2009-12-27 20:26:23 +0800

    overlay.js: Aggiunta licenza, autore e collaboratore

--- chrome/content/overlay.js
+++ chrome/content/overlay.js
@@ -1,14 +1,21 @@
 /*
-#
-#
-# You should have received a copy of the GNU General Public License
-# along with 'IngUnisannio'.  If not, see <http://www.gnu.org/licenses/>.
-#
-# Author: Stefano Viola < stefanoviola [at] sanniolug [dot] org >
-#
-# 
-#
-#
+This file is part of 'IngUnisannio'.
+
+'IngUnisannio' is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License,
+or any later version.
+
+'IngUnisannio' is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with 'IngUnisannio'.  If not, see <http://www.gnu.org/licenses/>.
+
+Author: Stefano Viola < stefanoviola [at] sanniolug [dot] org >
+Collaborator: Ivan Morgillo < imorgillo [at] sanniolug [dot] org >
 */
 
 //Global variables
